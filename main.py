cf0128be918e47e74342ea6b077ef3b815a38bf9
lenghanzhu <zhulenghan@126.com>
2023-02-17 14:45:35 +0000

    change image transform

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("{} is in use".format(device))
 
 data_transform = transforms.Compose(
-    [transforms.Resize([256, 256]),
-     transforms.CenterCrop(224),
+    [transforms.CenterCrop([256, 256]),
+     transforms.Resize(224),
      transforms.ToTensor(),
      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
