29772156a585b8ae7270a140906a4a9008139578
lenghanzhu <zhulenghan@126.com>
2023-02-24 14:20:31 +0000

    remove imshow and add control buttom

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ from PIL import Image
 import torch
 from torchvision import transforms
 from model import resnet18
-import json
 import time
 from com import send
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
+
+keyboard0 = Controller()
+
+isEnd = False
+
+
+def keyboard_on_release(key):
+    global isEnd
+    if key == keyboard.Key.esc:
+        isEnd = True
+        return False
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("{} is in use".format(device))
@@ -17,53 +29,66 @@ data_transform = transforms.Compose(
      transforms.ToTensor(),
      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
+
+net = resnet18(num_classes=7).to(device)
+net.load_state_dict(torch.load("weight/mms.pth", map_location=device))
+net.eval()
+
+
 device_exist = 0
 port_list = list(serial.tools.list_ports.comports())
 port_name = "STM32"
 for i in range(0, len(port_list)):
     if port_name in port_list[i].description:
-        serial = serial.Serial(port_list[i].device, 9600, write_timeout=2)
+        serial = serial.Serial(port_list[i].device)
         device_exist = 1
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-with open('./class_indices.json', "r") as f:
-    class_indict = json.load(f)
-
-net = resnet18(num_classes=6).to(device)
-net.load_state_dict(torch.load("weight/mms.pth", map_location=device))
-net.eval()
-
-start_time = time.time()
-x = 1  # displays the frame rate every 1 second
-counter = 0
-
+stopper = keyboard.Listener(on_release=keyboard_on_release)
 while 1:
 
-    ret, frame = cap.read()
-    cv2.imshow("capture", frame)
-    frame_PIL = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    image = torch.unsqueeze(data_transform(frame_PIL), dim=0)
+    with keyboard.Listener(
+            on_release=keyboard_on_release) as starter:
+        starter.join()
 
-    with torch.no_grad():
-        output = torch.squeeze(net(image.to(device))).cpu()
-        predict = torch.argmax(output).numpy()
+    stopper = keyboard.Listener(on_release=keyboard_on_release)
+    stopper.start()
+    isEnd = False
 
-    if device_exist:
-        send(serial, 1, predict)
-    print(class_indict[str(predict)])
 
-    if (cv2.waitKey(1) & 0xFF == ord('q')):
-        break
+    start_time = time.time()
+    x = 1  # displays the frame rate every 1 second
+    counter = 0
 
-    counter += 1
-    if (time.time() - start_time) > x:
-        '''
-        print("FPS: ", counter / (time.time() - start_time))
-        '''
-        counter = 0
-        start_time = time.time()
+    while 1:
 
-serial.close()
+        ret, frame = cap.read()
+        '''cv2.imshow("capture", frame)'''
+        frame_PIL = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        image = torch.unsqueeze(data_transform(frame_PIL), dim=0)
+
+        with torch.no_grad():
+            output = torch.squeeze(net(image.to(device))).cpu()
+            predict = torch.argmax(output).numpy()
+
+        if device_exist:
+            send(serial, 0, predict)
+
+        '''print(class_indict[str(predict)])'''
+
+        counter += 1
+        if (time.time() - start_time) >= x:
+
+            print("FPS: %.3f" % (counter / (time.time() - start_time)))
+
+            counter = 0
+            start_time = time.time()
+
+        if device_exist:
+            send(serial, 1, predict)
+
+        if isEnd:
+            break
